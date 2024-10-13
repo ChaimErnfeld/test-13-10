cbda371e3f09645954d9a8183f45685f462505e4
ChaimErnfeld <mp058328@gmail.com>
2024-10-13 17:03:36 +0300

    add swagger

--- src/routes/auteRoutes.ts
+++ src/routes/auteRoutes.ts
@@ -3,8 +3,84 @@ import { registerTeacher, registerStudent, login } from "../controllers/auteCont
 
 const auteRouter = Router();
 
+/**
+ * @swagger
+ * /api/teachers/registerTeacher:
+ *   post:
+ *     summary: Register a teacher
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               classroom:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Grade added successfully
+ *       400:
+ *         description: Cannot add grade to this classroom
+ */
 auteRouter.post("/registerTeacher", registerTeacher);
+
+/**
+ * @swagger
+ * /api/teachers/registerStudent:
+ *   post:
+ *     summary: Register a student
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               classroom:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Grade added successfully
+ *       400:
+ *         description: Cannot add grade to this classroom
+ */
 auteRouter.post("/registerStudent", registerStudent);
+
+/**
+ * @swagger
+ * /api/teachers/login:
+ *   post:
+ *     summary: login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Grade added successfully
+ *       400:
+ *         description: Cannot add grade to this classroom
+ */
 auteRouter.post("/login", login);
 
 export default auteRouter;
--- src/routes/teacherRoutes.ts
+++ src/routes/teacherRoutes.ts
@@ -3,9 +3,69 @@ import { addGrade, getStudents, editGrade } from "../controllers/teacherControll
 import { isTeacher } from "../middleware/isTeacher";
 
 const teacherRouter = Router();
-
+/**
+ * @swagger
+ * /api/teachers/grades:
+ *   post:
+ *     summary: Add a grade
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               classroom:
+ *                 type: string
+ *               studentId:
+ *                 type: string
+ *               grade:
+ *                 type: number
+ *               comment:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Grade added successfully
+ *       400:
+ *         description: Cannot add grade to this classroom
+ */
 teacherRouter.post("/grade", isTeacher, addGrade);
+
+/**
+ * @swagger
+ * /api/teachers//:
+ *   get:
+ *     summary: Get students
+ *     responses:
+ *       200:
+ *         description: List of students
+ */
 teacherRouter.get("/", getStudents);
+
+/**
+ * @swagger
+ * /api/teachers//grade:
+ *   put:
+ *     summary: Edit a grade
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               studentName:
+ *                 type: string
+ *               gradeId:
+ *                 type: string
+ *               newGrade:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Grade changed successfully
+ *       404:
+ *         description: Student or Grade not found
+ */
 teacherRouter.put("/grade", editGrade);
 
 export default teacherRouter;
